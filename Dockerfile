fcdcdb2f0258f4cfc013b5e5aeda02e4707102fc
Ivan Kurban <ivankurban1986@gmail.com>
2022-03-25 00:39:02 +0300

    Play doesn't work

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9
+RUN apt-get update -y
+RUN apt-get upgrade -y
+
+WORKDIR /app/music_bot
+
+COPY ./requirements.txt ./
+RUN pip install -r requirements.txt
+COPY ./ ./
+
+CMD ["python3", "start.py"]
---
+++ bot/bot.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from discord.ext import commands
+
+
+class MusicBot(commands.Bot):
+
+    def __init__(self):
+        self._cogs = [p.stem for p in Path(".").glob("./bot/cogs/*.py")]
+        super().__init__(command_prefix=self.prefix, case_insensitive=True)
+
+    def setup(self):
+        print("Running setup...")
+
+        for cog in self._cogs:
+            self.load_extension(f"bot.cogs.{cog}")
+            print(f" Loaded `{cog}` cog.")
+
+        print("Setup complete.")
+
+    def run(self):
+        self.setup()
+
+        with open("data/token.0", "r", encoding="utf-8") as f:
+            TOKEN = f.read()
+
+        print("Running bot...")
+        super().run(TOKEN, reconnect=True)
+
+    async def on_ready(self):
+        print('Bot is ready!')
+
+    async def prefix(self, bot, msg):
+        return commands.when_mentioned_or("!")(bot, msg)
